43b5670de82df52f673d3862f2e3659d0b0526d8
Marina <marinabevi4@gmail.com>
2026-05-21 12:35:03 +0200

    borrar init.modelo

--- src/models/__init__.py
+++
@@ -1,6 +0,0 @@
-# Solo importas los modelos de su propia carpeta
-from src.models.post import Post
-from src.models.respuesta import Respuesta
-
-# Opcional: puedes listarlos aquí para que al importar 'src.models' se carguen todos
-__all__ = ["Post", "Respuesta"]
