7e666c95ace29980121509f2ca3c8941cb65cfc9
karyoli <karyoli.ie@gmail.com>
2026-05-22 12:41:44 +0200

    feat: Add cookie security flags configuration to enhance session security

--- config.py
+++ config.py
@@ -1,8 +1,19 @@
 import os
 
+def _bool(env_value, default=False):
+    if env_value is None:
+        return default
+    return str(env_value).lower() in ('1', 'true', 'yes', 'on')
+
 class Config:
-    SECRET_KEY = os.environ.get('SECRET_KEY')
-    MYSQL_HOST = os.environ.get('MYSQL_HOST')
-    MYSQL_USER = os.environ.get('MYSQL_USER')
-    MYSQL_PASSWORD = os.environ.get('MYSQL_PASSWORD')
-    MYSQL_DB = os.environ.get('MYSQL_DB')
+    SECRET_KEY = os.environ.get('SECRET_KEY')  # En prod debe ser obligatoria
+    MYSQL_HOST = os.environ.get('MYSQL_HOST', 'localhost')
+    MYSQL_USER = os.environ.get('MYSQL_USER', 'root')
+    MYSQL_PASSWORD = os.environ.get('MYSQL_PASSWORD', '')
+    MYSQL_DB = os.environ.get('MYSQL_DB', 'abptest')
+
+    # Cookie security flags (se leen desde env y se convierten correctamente)
+    SESSION_COOKIE_SECURE = _bool(os.environ.get('SESSION_COOKIE_SECURE'), False)
+    SESSION_COOKIE_HTTPONLY = _bool(os.environ.get('SESSION_COOKIE_HTTPONLY'), True)
+    SESSION_COOKIE_SAMESITE = os.environ.get('SESSION_COOKIE_SAMESITE', 'Lax')
+    REMEMBER_COOKIE_SECURE = _bool(os.environ.get('REMEMBER_COOKIE_SECURE'), False)
