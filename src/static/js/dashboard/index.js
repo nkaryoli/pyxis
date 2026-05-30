d43238dbd4acd57fba4d2f70821bfcfd0ab16bfd
karyoli <karyoli.ie@gmail.com>
2026-05-30 18:51:43 +0200

    fix: cerrar menu dropdown al hacer click fuera de ellos

--- src/static/js/dashboard/index.js
+++ src/static/js/dashboard/index.js
@@ -9,5 +9,14 @@ if (context) {
     initClientPagination("seccion-modulos", 7);
     initClientPagination("seccion-usuarios", 7);
     initClientPagination("seccion-posts", 7);
+    document.addEventListener("click", (e) => {
+        const target = e.target;
+        const openDetails = document.querySelectorAll("details[open]");
+        openDetails.forEach((details) => {
+            if (!details.contains(target)) {
+                details.open = false;
+            }
+        });
+    });
 }
 //# sourceMappingURL=index.js.map
--- ts_source/dashboard/index.ts
+++ ts_source/dashboard/index.ts
@@ -12,4 +12,14 @@ if (context) {
 	initClientPagination("seccion-modulos", 7);
 	initClientPagination("seccion-usuarios", 7);
 	initClientPagination("seccion-posts", 7);
+
+	document.addEventListener("click", (e) => {
+		const target = e.target as Element;
+		const openDetails = document.querySelectorAll<HTMLDetailsElement>("details[open]");
+		openDetails.forEach((details) => {
+			if (!details.contains(target)) {
+				details.open = false;
+			}
+		});
+	});
 }
